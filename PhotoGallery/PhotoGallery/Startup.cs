c9e268b8e0ead8969687a1ad369e37ce40a9c2a5
domgec <dominykas.gecas@gmail.com>
2023-10-17 08:12:14 +0300

    Fix issues after analysis

--- PhotoGallery/PhotoGallery/Startup.cs
+++ PhotoGallery/PhotoGallery/Startup.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using PhotoGallery.Auth;
@@ -17,11 +14,7 @@ using PhotoGallery.Models;
 using PhotoGallery.Repositories;
 using PhotoGallery.Repositories.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
 namespace PhotoGallery
@@ -41,10 +34,7 @@ namespace PhotoGallery
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<PhotoGalleryDBContext>()
                 .AddDefaultTokenProviders();
-            services.AddCors(options =>
-            {
-                options.AddPolicy("all", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-            });
+
             services.AddAuthentication(o =>
             {
                 o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
